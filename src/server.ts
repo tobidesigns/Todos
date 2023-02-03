422dea15d957cc128b7f08e00562c5c87222b36b
Tobi <tobiraheem@gmail.com>
2023-02-03 17:35:14 +0000

    Set up express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("server running.....")
+import express, {Request, Response} from 'express';
+
+const app = express();
+const port = 9000;
+
+app.get('/', (req: Request, res: Response) => {
+    return res.send('hello world');
+});
+
+app.listen(port, ()=> {
+    console.log('Server is running on port ' + port)
+});
